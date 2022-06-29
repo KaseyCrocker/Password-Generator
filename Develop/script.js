6cee0999397ab4389f1f3f6301b7a194eaf73d2a
Kasey <kasey_crocker@yahoo.com>
2022-06-29 00:00:30 -0400

    added variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,29 +1,30 @@
 // Assignment code here
 
-// psuedocode
-
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-generatePassword = function () {
-  prompt (
-    "How many charcacter would you like there to be?"
-  );
-};
-
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+    var symbols = ("!@#$%^&*");
+    var numbers = ("0123456789");
+    var lowerCase = ("abcdefghijklmnopqrstuvwxyz");
+    var upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    var combined = symbols + numbers + lowerCase + upperCase;
+    var passwordLength = window.prompt("How many charcaters would you like in your password?")
+    if (passwordLength >= 8 && passwordLength <= 128) {
+      }
+    else {
+      window.alert("You need to provide a valid answer! Please try again.")
+      return writePassword()
+    };
+    var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * combined.length);
+    password += combined.substring(randomNumber, randomNumber +1);
+   }
+
   var passwordText = document.querySelector("#password");
-  
-  
-
-  
-
   passwordText.value = password;
-
   
 
 };
